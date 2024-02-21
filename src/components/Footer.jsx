d26bce4ecc1083213115a56aaebc365537bf041a
Nabil-Pathan <nabilkhan9624@gmail.com>
2024-02-21 10:10:44 +0530

    Footer Modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "../images/logo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook ,faTwitter , faInstagram , faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 const Footer = () => {
   return (
-    <footer className="bg-customPurple  w-full ">
-      <div className=" flex flex-col w-full  md:items-baseline items-center p-6 md:p-10">
+    <footer className="bg-customPurple w-full">
+      <div className="flex flex-col w-full  items-center p-6 ">
         {/* Footer columns */}
-        <div className="grid p-4 grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid p-4  w-full grid-cols-1 md:grid-cols-4 gap-8 justify-center items-center">
           {/* Column 1 */}
-          <div>
+          <div className='text-center md:p-0 p-6'>
             <h2 className="text-lg font-semibold mb-4">Primary Pages</h2>
             <ul>
               <li><Link to="/aboutus">About Us</Link></li>
@@ -20,29 +20,27 @@ const Footer = () => {
               <li><Link to="/">Team Details</Link></li>
             </ul>
           </div>
-          
+
           {/* Column 2 */}
-          <div>
+          <div  className='text-center md:p-0 p-6'>
             <h2 className="text-lg font-semibold mb-4">Pages</h2>
             <ul>
               <li>
                 <Link to="/creative-agency">Creative Agency</Link>
-                </li>
-
-                <li>
+              </li>
+              <li>
                 <Link to="/portfolio">Personal Portfolio</Link>
-                </li>
-
-                <li>
-                <Link to="/digital-agancy">Digital Agancy</Link>
-                </li>
+              </li>
+              <li>
+                <Link to="/digital-agency">Digital Agency</Link>
+              </li>
             </ul>
           </div>
-          
+
           {/* Column 3 */}
-          <div>
-            <h2 className="text-lg font-semibold mb-4">Resources</h2>
-            <ul>
+          <div  className='text-center md:p-0 p-6'>
+            <h2 className="text-lg font-semibold ">Resources</h2>
+            <ul >
               <li><a href="#">Blog</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Support</a></li>
@@ -50,9 +48,9 @@ const Footer = () => {
               <li><a href="#">Building Business</a></li>
             </ul>
           </div>
-          
+
           {/* Column 4 */}
-          <div>
+          <div  className='text-center md:p-0 p-6'>
             <h2 className="text-lg font-semibold mb-4">News</h2>
             <ul>
               <li><a href="#">Events</a></li>
@@ -61,22 +59,22 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        
+
         {/* Second row */}
-        <div className="flex md:flex-row flex-col gap-6 justify-between items-center mt-8 border-t border-gray-700 pt-8">
+        <div className="flex md:flex-row w-full md:p-20 flex-col gap-6 justify-between items-center mt-8 border-t border-gray-700 pt-8">
           {/* Logo and social icons */}
-          <div>
+          <div className="flex flex-col items-center">
             <img src={Logo} alt="Logo" className="h-10 mb-4" />
             <div className="flex space-x-4">
-            <FontAwesomeIcon className='text-3xl' icon={faLinkedin} />
-            <FontAwesomeIcon className='text-3xl' icon={faInstagram} />
-              <FontAwesomeIcon className='text-3xl' icon={faFacebook} />
-              <FontAwesomeIcon className='text-3xl' icon={faTwitter} />
+              <FontAwesomeIcon className="text-3xl" icon={faLinkedin} />
+              <FontAwesomeIcon className="text-3xl" icon={faInstagram} />
+              <FontAwesomeIcon className="text-3xl" icon={faFacebook} />
+              <FontAwesomeIcon className="text-3xl" icon={faTwitter} />
             </div>
           </div>
-          
+
           {/* Contact details */}
-          <div>
+          <div className="flex flex-col items-center">
             <h2 className="text-lg font-semibold mb-4">Contact Us</h2>
             <p>123 Main Street, City, Country</p>
             <p>info@example.com</p>
